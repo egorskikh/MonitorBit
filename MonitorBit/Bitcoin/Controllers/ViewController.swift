bd36573d16797c4161c7c408a9a3fe8a85ee4fa9
Egor Gorskikh <egorgorskikh@gmail.com>
2021-03-30 09:19:16 +0300

    Ability to add cells

--- MonitorBit/Bitcoin/Controllers/ViewController.swift
+++ MonitorBit/Bitcoin/Controllers/ViewController.swift
@@ -14,16 +14,32 @@ class ViewController: UIViewController {
     let networkDataFetcher = NetworkDataFetcher()
     let btcCell = BTCCell()
     
+    var resultsUPD = [String]()
+    var resultsEUR = [String]()
+    var resultsRUB = [String]()
+    var dateResults = [String]()
+    
+    lazy var formatteddate: String = {
+        let time = NSDate()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd-MM"
+        let formatteddate = formatter.string(from: time as Date)
+        return formatteddate
+    }()
+    
     @IBOutlet weak var updLabel: UILabel!
     @IBOutlet weak var eurLabel: UILabel!
     @IBOutlet weak var rubLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btcEqual: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     // MARK: - View Life Cicle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        fetchDataBTC()
         setupTableView()
     }
     
@@ -31,19 +47,24 @@ class ViewController: UIViewController {
     
     @IBAction func add(_ sender: UIBarButtonItem) {
         
+        guard
+            let saveUpdLabetl = updLabel.text,
+            let saveEurLabetl = eurLabel.text,
+            let saveRubLabetl = rubLabel.text,
+            let dateLabel = dateLabel.text
+        else {
+            return
+        }
+        
+        self.resultsUPD.append(saveUpdLabetl)
+        self.resultsEUR.append(saveEurLabetl)
+        self.resultsRUB.append(saveRubLabetl)
+        self.dateResults.append(dateLabel)
+        
         tableView.reloadData()
     }
     
-    @IBAction func refresh(_ sender: UIBarButtonItem) {
-        
-        networkDataFetcher.fetchСourse { (fetchСourse) in
-            guard let course = fetchСourse else { return }
-            self.eurLabel.text = String(course.EUR.buy) + " " + String(course.EUR.symbol)
-            self.updLabel.text = String(course.USD.buy) + " " + String(course.USD.symbol)
-            self.rubLabel.text = String(course.RUB.buy) + " " + String(course.RUB.symbol)
-        }
-        
-    }
+    
     
 }
 
@@ -52,26 +73,45 @@ class ViewController: UIViewController {
 extension ViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-      "price history"
+        "price history"
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        1
+        resultsUPD.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: BTCCell.reuseID, for: indexPath) as! BTCCell
+        cell.updLabel.text = resultsUPD[indexPath.row]
+        cell.eurLabel.text = resultsEUR[indexPath.row]
+        cell.rubLabel.text = resultsRUB[indexPath.row]
+        cell.dateLabel.text = dateResults[indexPath.row]
         return cell
     }
     
 }
 
-// MARK: - Setup Setup Method
+// MARK: - Setup Method
 
 extension ViewController {
     
     private func setupTableView() {
         tableView.rowHeight = 91
+        title = "1 BTC"
+        navigationController?.navigationBar.prefersLargeTitles = true
+    }
+    
+    private func fetchDataBTC() {
+        
+        networkDataFetcher.fetchСourse { (fetchСourse) in
+            guard let course = fetchСourse else { return }
+            self.eurLabel.text = String(course.EUR.buy) + " " + String(course.EUR.symbol)
+            self.updLabel.text = String(course.USD.buy) + " " + String(course.USD.symbol)
+            self.rubLabel.text = String(course.RUB.buy) + " " + String(course.RUB.symbol)
+            self.btcEqual.text = "1 BTC ="
+            self.dateLabel.text = self.formatteddate
+        }
+        
     }
     
 }
