cb216a92f75e0b4186ac18a449e5e9f1a00bcc77
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-24 15:52:06 +0300

    feat: get rid of the storyboard entirely.

--- MonitorBit/Bitcoin/View/BtcCell.swift
+++ MonitorBit/Bitcoin/View/BtcCell.swift
@@ -9,18 +9,82 @@ import UIKit
 
 class BtcCell: UITableViewCell {
     
-    static let reuseID = "BTCCell"
+    static let reuseID = "Cell"
     
-    @IBOutlet weak var updLabel: UILabel!
-    @IBOutlet weak var eurLabel: UILabel!
-    @IBOutlet weak var rubLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+    lazy var stackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [ updLbl,
+                                                        eurLbl,
+                                                        rubLbl] )
+        stackView.axis = .vertical
+        stackView.distribution = .fill
+        stackView.alignment = .fill
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.spacing = 3
+        stackView.contentMode = .scaleToFill
+        return stackView
+    }()
+    
+    lazy var updLbl: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var eurLbl: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var rubLbl: UILabel = {
+        let label = UILabel()
+        return label
+    }()
+    
+    lazy var dateLbl: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    fileprivate var view: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupConstraint()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    func setupConstraint() {
+        contentView.addSubview(view)
+        view.addSubview(stackView)
+        view.addSubview(dateLbl)
+        
+        NSLayoutConstraint.activate([
+            view.topAnchor.constraint(equalTo: contentView.topAnchor),
+            view.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            view.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+            
+            stackView.leadingAnchor.constraint(equalTo: view.layoutMarginsGuide.leadingAnchor, constant: 10),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            dateLbl.leadingAnchor.constraint(greaterThanOrEqualTo: stackView.trailingAnchor),
+            dateLbl.trailingAnchor.constraint(equalTo: view.layoutMarginsGuide.trailingAnchor),
+            dateLbl.bottomAnchor.constraint(equalTo: view.layoutMarginsGuide.bottomAnchor)
+        ])
+    }
     
     func configuratinCell(_ btc: BTC) {
-        self.updLabel.text = btc.upd
-        self.eurLabel.text = btc.eur
-        self.rubLabel.text = btc.rub
-        self.dateLabel.text = btc.date
+        self.updLbl.text = btc.upd
+        self.eurLbl.text = btc.eur
+        self.rubLbl.text = btc.rub
+        self.dateLbl.text = btc.date
     }
     
 }
