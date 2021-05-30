7c2dd8206a9a4a09e7487b8d824b937595c97615
Egor Gorskikh <60622982+egorskikh@users.noreply.github.com>
2021-05-30 17:04:31 +0300

    feat: add all currencies

--- MonitorBit/BitcoinModule/Models/BitcoinResponse.swift
+++ MonitorBit/BitcoinModule/Models/BitcoinResponse.swift
@@ -11,6 +11,26 @@ struct BitcoinResponse: Decodable {
     let USD: Details
     let EUR: Details
     let RUB: Details
+    let AUD: Details
+    let BRL: Details
+    let CAD: Details
+    let CHF: Details
+    let CLP: Details
+    let CNY: Details
+    let DKK: Details
+    let GBP: Details
+    let HKD: Details
+    let INR: Details
+    let ISK: Details
+    let JPY: Details
+    let KRW: Details
+    let NZD: Details
+    let PLN: Details
+    let SEK: Details
+    let SGD: Details
+    let THB: Details
+    let TRY: Details
+    let TWD: Details
 }
 
 struct Details: Decodable {
